a33bc3d6913a8bcfdf61035c64a8fb48faba87c5
Joelson Junior <53396470+joelsonejr@users.noreply.github.com>
2021-12-23 18:51:21 -0300

    Components SideBar and Content sucessfully created and implemented

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -2,12 +2,9 @@ import {useState, useEffect} from 'react';
 import {api} from '../services/api';
 
 import {Button} from '../components/Button';
-import {MovieCard} from '../components/MovieCard';
-
+import { Content } from '../components/Content';
 
 import '../styles/sidebar.scss';
-import '../styles/content.scss';
-
 
 interface GenreResponseProps {
   id: number;
@@ -15,48 +12,24 @@ interface GenreResponseProps {
   title: string;
 }
 
-interface MovieProps {
-  imdbID: string;
-  Title: string;
-  Poster: string;
-  Ratings: Array<{
-    Source: string;
-    Value: string;
-  }>;
-  Runtime: string;
-}
-
-
 export function SideBar() {
   const [selectedGenreId, setSelectedGenreId] = useState(1);
-
   const [genres, setGenres] = useState<GenreResponseProps[]>([]);
 
-  const [movies, setMovies] = useState<MovieProps[]>([]);
-  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
-
+  
   useEffect(() => {
     api.get<GenreResponseProps[]>('genres').then(response => {
       setGenres(response.data);
     });
   }, []);
 
-  useEffect(() => {
-    api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
-      setMovies(response.data);
-    });
-
-    api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
-      setSelectedGenre(response.data);
-    })
-  }, [selectedGenreId]);
-  // TODO: Início do Sidebar
+  
   function handleClickButton(id: number) {
     setSelectedGenreId(id);
   }
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'row' }}>
+    <>
       <nav className="sidebar">
         <span>Watch<p>Me</p></span>
 
@@ -71,23 +44,9 @@ export function SideBar() {
             />
           ))}
         </div>
-
       </nav>
-      {/* Final do sibebar */}
-      {/* TODO: Início do Content */}
-      <div className="container">
-  <header>
-    <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-  </header>
-
-  <main>
-    <div className="movies-list">
-      {movies.map(movie => (
-        <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-      ))}
-    </div>
-  </main>
-</div>
-    </div>
+      
+      <Content selectedGenreId={selectedGenreId} />
+      </>
   )
 }
